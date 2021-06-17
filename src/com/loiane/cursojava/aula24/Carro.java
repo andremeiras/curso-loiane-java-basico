3451f8b10714e9a48a1ce41871b9fe8a25d19c30
andremeiras <andremeirati@gmail.com>
2021-06-16 23:38:05 -0300

    Add aula 24 e 25 do curso de Java básico. Foi mostrado como criar uma classe com métodos simples e depois como realizar testes da classe criada fazendo a instanciação dela na classe de teste.

---
+++ src/com/loiane/cursojava/aula24/Carro.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula24;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros; // número de passageiros
+	double capCombustivel; // capacidade do tanque de combustível
+	double consumoCombustível; // consumo de combustível por km
+}
---
+++ src/com/loiane/cursojava/aula25/Carro.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula25;
+// Curso Java Básico - Aula 25
+// Título: Orientação a Objetos: Classes e métodos simples
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	void exibirAutonomia(){
+		System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + " km");
+	}
+}
