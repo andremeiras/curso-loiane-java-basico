c887833d1055e02217aafe0256fb60b8a2ed39e6
andremeiras <andremeirati@gmail.com>
2021-06-16 23:50:53 -0300

    Adicionado aula26 mostrando método com retorno do tipo double.

--- src/com/loiane/cursojava/basico/aula26/Carro.java
+++ src/com/loiane/cursojava/basico/aula26/Carro.java
@@ -1,5 +1,23 @@
 package com.loiane.cursojava.basico.aula26;
 
 public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	// método sem retorno
+	void exibirAutonomia() {
+		// utilizando o método que faz o cálculo da autonomia
+		System.out.println("A autonomia do carro é: " + obterAutonomia() + " km");
+	}
+	
+	// criando método com retorno
+	double obterAutonomia() {
+		System.out.println("Método obterAutonomia foi chamado.");
+		return capCombustivel * consumoCombustivel;
+	}
 
 }
